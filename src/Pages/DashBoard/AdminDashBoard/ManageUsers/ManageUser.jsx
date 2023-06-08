dc58d11f1604fd5649baa156914386477579146f
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-06-08 19:32:44 +0600

    user make admin and instructor

--- src/Pages/DashBoard/AdminDashBoard/ManageUsers/ManageUser.jsx
+++ src/Pages/DashBoard/AdminDashBoard/ManageUsers/ManageUser.jsx
@@ -1,19 +1,110 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
-    const [regUser, setRegUser] = useState([]);
+  const [axiosSecure] = useAxiosSecure();
+  const { data: regUsers = [], refetch } = useQuery(["users"], async () => {
+    const res = await axiosSecure.get("/users");
+    return res.data;
+  });
+  const handleUpdateAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You Want to update this  ${user?.name} as Admin`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Update As A Admin",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/admin/${user?._id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.modifiedCount > 0) {
+              Swal.fire("Updated!", "Your user now admin.", "success");
+              refetch();
+            }
+          });
+      }
+    });
+  };
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/users')
-        .then(res=>res.json())
-        .then(data=>setRegUser(data))
-    },[])
-    console.log(regUser)
-    return (
-        <div>
-            <p>This is manage users</p>
-        </div>
-    );
+  const handleUpdateInstructor = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You Want to update   ${user?.name} as Instructor `,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Update As A Instructor",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/instructor/${user?._id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.modifiedCount > 0) {
+              Swal.fire("Updated!", "Your user now Instructor.", "success");
+              refetch();
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="w-full mx-10">
+      <p className="text-4xl font-bold m-5">Total Users : {regUsers?.length}</p>
+      <div className="overflow-x-auto  ">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {regUsers?.map((regUser, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{regUser?.name}</td>
+                <td>{regUser?.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleUpdateAdmin(regUser)}
+                    className="btn btn-primary"
+                    disabled={regUser?.role === "admin"}
+                  >
+                    Make Admin
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleUpdateInstructor(regUser)}
+                    className="btn btn-accent"
+                    disabled={regUser?.role ==='instructor'}
+                  >
+                    Make Instructor
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageUser;
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
+      <QueryClientProvider client={queryClient}>
       <div>
         <RouterProvider router={router} />
       </div>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
